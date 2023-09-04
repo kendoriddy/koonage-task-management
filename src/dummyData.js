046c67718282a5acf0e5b419e5a15aaf1f44d999
Kehinde Onifade <onifkay@gmail.com>
2023-09-04 22:16:55 +0100

    added dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,34 @@
+const dummyTasks = [
+  {
+    id: 1,
+    name: "Complete Project Proposal",
+    description: "Finish the proposal document for the new project.",
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "Meeting with Team",
+    description: "Attend the weekly team meeting at 10 AM.",
+    completed: true,
+  },
+  {
+    id: 3,
+    name: "Write Code Documentation",
+    description: "Document the codebase for the current project.",
+    completed: false,
+  },
+  {
+    id: 4,
+    name: "Prepare Presentation",
+    description: "Create a presentation for the upcoming client meeting.",
+    completed: false,
+  },
+  {
+    id: 5,
+    name: "Review Test Cases",
+    description: "Review and update the test cases for the application.",
+    completed: true,
+  },
+];
+
+export default dummyTasks;
