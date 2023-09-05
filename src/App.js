6d9f9db9b4b34294f7a19820daa30da8177ab8b9
Kehinde Onifade <onifkay@gmail.com>
2023-09-05 09:33:17 +0100

    implement filter functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import TaskFormPopup from "./components/TaskFormPopup";
 import TaskList from "./components/TaskList";
 import dummyData from "./dummyData";
+import TaskLoading from "./components/TaskLoading";
+import TaskErrorMessage from "./components/TaskErrorMessage";
 
 function App() {
   const [isFormPopupOpen, setFormPopupOpen] = useState(false);
   const [tasks, setTasks] = useState(dummyData);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [filter, setFilter] = useState("all"); //
+  const [filter, setFilter] = useState("all");
 
   const handleAddTask = (newTask) => {
     const newTaskWithId = { ...newTask, id: Date.now() };
@@ -40,15 +42,24 @@ function App() {
     const updatedTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(updatedTasks);
   };
+
+  const handleFilterChange = (selectedFilter) => {
+    setFilter(selectedFilter);
+  };
+
   console.log(tasks);
   return (
     <div className="App">
       <h1>Task Management App</h1>
+      {loading && <TaskLoading />}
+      {error && <TaskErrorMessage error={error} />}
       <TaskList
         tasks={tasks}
         openFormPopup={openFormPopup}
         changeTaskStatus={changeTaskStatus}
         onDeleteTask={handleDeleteTask}
+        filter={filter}
+        handleFilterChange={handleFilterChange}
       />
       {isFormPopupOpen && (
         <TaskFormPopup
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import TaskItem from "./TaskItem";
 
-const TaskList = ({ tasks, openFormPopup, changeTaskStatus, onDeleteTask }) => {
+const TaskList = ({
+  tasks,
+  openFormPopup,
+  changeTaskStatus,
+  onDeleteTask,
+  filter,
+  handleFilterChange,
+}) => {
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "all") {
+      return tasks;
+    } else if (filter === "completed") {
+      return task.completed;
+    } else {
+      return !task.completed;
+    }
+  });
   return (
     <div>
       <div>
         <h2>Task List</h2>
         <button onClick={openFormPopup}>Add Task</button>
+        <select onChange={(e) => handleFilterChange(e.target.value)}>
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="incomplete">Incomplete</option>
+        </select>
       </div>
       <ul>
-        {tasks.map((task) => (
+        {filteredTasks.map((task) => (
           <TaskItem
             key={task.id}
             task={task}
